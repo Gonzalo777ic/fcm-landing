c7ebedd24f207c8eefff2d8ca2fb8bc39f289a8f
Gonzalo Isique <gonzaloisique777@gmail.com>
2026-03-11 12:20:09 -0500

    envío a correo

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
 
     await resend.emails.send({
       from: 'Web FCM <onboarding@resend.dev>',
-      to: ['megelmi@gmail.com'],
+      to: ['mgelmi@gmail.com'],
       subject: `Nuevo mensaje de: ${nombre}`,
       replyTo: email,
       html: `
